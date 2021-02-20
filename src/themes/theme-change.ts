eea82235242aa0c811e3f7f3d825053274891804
Matheus Ale <m.matheus.ale@gmail.com>
2021-02-20 16:09:32 -0300

    use lazy-theme-changer

--- src/themes/theme-change.ts
+++ src/themes/theme-change.ts
@@ -1,3 +1,5 @@
+import lazyThemeChanger from 'lazy-theme-changer';
+
 const themes = {
     dark: () => import(
         /* webpackChunkName: "theme-dark" */
@@ -19,49 +21,8 @@ const themes = {
         ),
 };
 
-export type Theme =  keyof typeof themes;
+export type Theme =  keyof typeof themes | string;
 
 export function themeChanger() {
-
-
-    const getStyle = (theme: Theme): HTMLStyleElement | null => {
-        return document.querySelector<HTMLStyleElement>(`style[data-theme*=theme-${theme}],link[href*=theme-${theme}]:not([rel="prefetch"])`);
-    };
-
-    const toggleStyle = (style: HTMLStyleElement, state: boolean) => {
-        if (state) {
-            style.removeAttribute('media');
-        } else {
-            style.setAttribute('media', 'max-width: 1px');
-        }
-    };
-
-    const disableOthersStyles = (theme: Theme) => {
-        const styles = document.querySelectorAll<HTMLStyleElement>(`
-            style[data-theme*=theme]:not([data-theme*=theme-${theme}]),
-            link[href*=theme]:not([href*=theme-${theme}]):not([rel="prefetch"])
-        `);
-        styles.forEach(style => toggleStyle(style, false));
-    };
-
-    const fetch = async (theme: Theme) => {
-        const styles = new Set(document.querySelectorAll('style:not([url~=""])'));
-        await themes[theme]();
-        let themeStyle = Array.from(document.querySelectorAll('style')).filter(s => !styles.has(s))
-            .find(s => s.innerText.includes(`/**@theme:${theme}*/`))
-        if (themeStyle) {
-            themeStyle.dataset.theme = `theme-${theme}`;
-        }
-    }
-
-    return async (theme: Theme) => {
-        const themeStyle = getStyle(theme);
-        if (themeStyle) {
-            toggleStyle(themeStyle, true);
-            disableOthersStyles(theme);
-            return;
-        }
-        await fetch(theme);
-        disableOthersStyles(theme);
-    };
+    return lazyThemeChanger(themes);
 }
